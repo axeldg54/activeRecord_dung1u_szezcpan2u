2f90f260a172234d2f2d60a2de91adc2b63a0349
axeldg54 <dung1u@etu.univ-lorraine.fr>
2023-11-21 15:00:37 +0100

    DBConnection

--- src/activeRecord/DBConnection.java
+++ src/activeRecord/DBConnection.java
@@ -9,27 +9,28 @@ public abstract class DBConnection {
 
     private Properties connectionProps;
     private static Connection connect;
-    private String userName, password, serverName, portNumber, tableName, dbName;
+    private String userName, password, serverName, portNumber, tableName, dbName, urlDB;
+
     public DBConnection(String userName, String password, String serverName, String portNumber, String tableName, String dbName) throws SQLException {
-        if (connect == null) {
-            this.userName = userName;
-            this.password = password;
-            this.serverName = serverName;
-            this.portNumber = portNumber;
-            this.tableName = tableName;
-            this.dbName = dbName;
+        this.userName = userName;
+        this.password = password;
+        this.serverName = serverName;
+        this.portNumber = portNumber;
+        this.tableName = tableName;
+        this.dbName = dbName;
 
-            connectionProps = new Properties();
-            connectionProps.put("user", userName);
-            connectionProps.put("password", password);
-            String urlDB = "jdbc:mysql://" + serverName + ":";
-            urlDB += portNumber + "/" + dbName;
-            connect = DriverManager.getConnection(urlDB,connectionProps);
-        }
+        connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+        urlDB = "jdbc:mysql://" + serverName + ":";
+        urlDB += portNumber + "/" + dbName;
+        this.connect = DriverManager.getConnection(urlDB, connectionProps);
     }
+
     public static synchronized Connection getConnection() {
-        return connect;
+        if (connect == null) new DBConnection();
     }
 
-    public void setNomDB(String nomDB) {}
+    public void setNomDB(String nomDB) {
+    }
 }
