f17179a0e6d289323ded1868f5a111ee6836fcad
axeldg54 <dung1u@etu.univ-lorraine.fr>
2023-11-27 10:57:01 +0100

    film

---
+++ src/activeRecord/Film.java
@@ -0,0 +1,11 @@
+package activeRecord;
+
+public class Film {
+    private String titre;
+    private int id;
+    private int id_real;
+
+    public Film(String titre, Personne personne) {
+        this.titre = titre;
+    }
+}
